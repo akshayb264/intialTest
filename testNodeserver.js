b8d396f1860061bb0f50fca865bd0fcdcc20e935
akshay <akshayb264@gmail.com>
2015-07-19 15:45:29 +0530

    Update testNodeserver.js

--- testNodeserver.js
+++ testNodeserver.js
@@ -1,77 +1,3 @@
- /* var x = {
-movie:"Rangatiranga"
-}
-  var  y =JSON.stringify(x);
-
-var http = require('http');
-var url = require('url');
-
-
-http.createServer(function (request, response) { 
-        var store = '';
-
-
-    request.on('end', function() {
-       
-        console.log("Came Here" +  request.url);
-            console.log(store.var1);
-            });
-    request.on('data', function(data) 
-    {
-        store='';
-        store = JSON.parse(data);
-    });
-    
-var sending_object = {
-names:[]
-};
-    
-    var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'emp'
-});
-
-connection.connect();
-
-connection.query('SELECT * from employee', function(err, rows, fields) {
-  if (err) throw err;
-
-    
-    for(var i=0;i<rows.length;i++){
-        sending_object.names.push(rows[i].name);
-    console.log('The solution is: ', rows[i].name);
-}
-
-  
-
-
-
-response.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-});
-
-    console.log(sending_object.names.length);
-        
-
-  response.end(JSON.stringify(sending_object));  
-});
-    connection.end();
-}).listen(3000);
-
-
-
-
-
-
-
-
-console.log("Server is listening"); */
 
 var express = require('express');
 var app = express();
@@ -84,9 +10,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.post('/',function(req,res){
-    console.log("Two Times");
     var x = {
-movie:"Rangatiranga"
+movie:"YYY"
 }
   var  y =JSON.stringify(x);
     
@@ -97,8 +22,8 @@ names:[]
     var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
-  password : 'root',
+  user     : 'username',
+  password : 'secret',
   database : 'emp'
 });
 
